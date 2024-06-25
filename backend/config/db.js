61a9b631e668b4f557d43cc205dd7850fa79f6c0
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-25 12:23:11 +0530

    Created db connnection

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MONGODB CONNECTED SUCCESSFULLY !! HOST ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MONGODB CONNECTION ERROR ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
