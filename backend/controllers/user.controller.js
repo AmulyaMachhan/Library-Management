ffd75ebd6228767940dacc4ee7c7cab5657fc205
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-25 17:39:10 +0530

    Created user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+export const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
