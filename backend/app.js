dd5b8ca3f6bc0fb5f3e67cb8cb5a78600ac9fd04
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-26 12:07:18 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //Routes Import
-import userRoutes from "./routes/user.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 //Routes Declaration
 app.use("/api/v1/users", userRouter);
