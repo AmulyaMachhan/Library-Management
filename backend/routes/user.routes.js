d4747d4e0e28b4b9442a2034d510bc56c7d1307d
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-26 14:22:10 +0530

    Updated user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers } from "../controllers/user.controller";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
