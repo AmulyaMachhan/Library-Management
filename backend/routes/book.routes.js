0ebb2076b02d688a60763920aead8dd5ead7c499
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-27 20:50:09 +0530

    Created global search route

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getAllBooks,
+  globalBookSearch,
   searchBooksByName,
   searchBooksByRent,
 } from "../controllers/book.controller.js";
@@ -10,5 +11,6 @@ const router = express.Router();
 router.route("/allbooks").get(getAllBooks);
 router.route("/search/name").get(searchBooksByName);
 router.route("/search/rent").get(searchBooksByRent);
+router.route("/search/global").get(globalBookSearch);
 
 export default router;
