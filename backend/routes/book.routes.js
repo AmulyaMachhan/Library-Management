8761421b30a0d94b30605c4930cf46dfa3b106ff
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-27 14:57:22 +0530

    Updated the search by rent router

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllBooks,
   searchBooksByName,
+  searchBooksByRent,
 } from "../controllers/book.controller.js";
 
 const router = express.Router();
 
 router.route("/allbooks").get(getAllBooks);
 router.route("/search/name").get(searchBooksByName);
+router.route("/search/rent").get(searchBooksByRent);
 
 export default router;
