b488ba6e6a8f75c9d0fdb170381f1c38412d8a40
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-27 13:16:45 +0530

    Updated book routes

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { getAllBooks } from "../controllers/book.controller.js";
 
 const router = express.Router();
 
+router.route("/allbooks").get(getAllBooks);
 export default router;
