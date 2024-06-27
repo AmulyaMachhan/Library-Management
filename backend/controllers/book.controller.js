12e16af040ba8af4b3b6d9b3fd6da12a79decbd0
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-27 13:29:45 +0530

    Created a controller for searching books

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -9,3 +9,13 @@ export const getAllBooks = asyncHandler(async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+export const searchBooksByName = asyncHandler(async (req, res) => {
+  const { query } = req.query;
+  try {
+    const books = await Book.find({ name: { $regex: query, $options: "i" } });
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
