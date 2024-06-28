709c1ca14ee7090771dd9d087c6bc2bb30f686c7
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-28 16:22:10 +0530

    Created route for returning a book

--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { issueBook } from "../controllers/transaction.controller.js";
+import {
+  issueBook,
+  returnBook,
+} from "../controllers/transaction.controller.js";
 
 const router = express.Router();
 
 router.route("/issue").post(issueBook);
+router.route("/return").post(returnBook);
+
 export default router;
