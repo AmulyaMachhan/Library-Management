f3816c38f63e4377650aebfeb2f65a22b69acd50
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-02 11:34:17 +0530

    Configured store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+
+  devTools: true,
+});
+
+setupListeners(store.dispatch);
+
+export default store;
