b3b08bebbc816b1d48f4cebb539169ce42504fcb
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-05 00:12:45 +0530

    Updated app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import Navigation from "./components/Layout/Navigation";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <>
       <ToastContainer />
+      <Header />
+      <Navigation />
       <main>
         <Outlet />
       </main>
