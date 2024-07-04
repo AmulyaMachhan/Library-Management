f0994bdc20a72f6275bfe1a7d3b86177a7faf0e5
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-04 13:39:25 +0530

    Provided store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+//React Redux
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
