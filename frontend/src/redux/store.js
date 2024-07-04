7a0094bb54b0d94edfe0c2fdcc1cf0f3e8fe470d
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-04 13:11:56 +0530

    Created store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { apiSlice } from "./api/apiSlice";
+import usersReducer from "./features/userSlice";
+import booksReducer from "./features/bookSlice";
+import transactionsReducer from "./features/transactionSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    users: usersReducer,
+    books: booksReducer,
+    transactions: transactionsReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
