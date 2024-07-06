d716c7b0157fdf08d6ef02d33748eab306b07231
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-06 13:22:50 +0530

    Updated routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,14 +10,15 @@ import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./components/Home/Home.jsx";
-import UserList from "./components/Users/UserList.jsx";
+
 import BookList from "./components/Books/BookList.jsx";
+import Users from "./components/Users/Users.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
-      <Route path="/userlist" element={<UserList />} />
+      <Route path="/users" element={<Users />} />
       <Route path="/booklist" element={<BookList />} />
     </Route>
   )
