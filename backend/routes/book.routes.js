b0766bec65465f45fa135e0934de207618c90feb
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-15 14:23:19 +0530

    Created a new route

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getAllBooks,
+  getAllCategories,
   globalBookSearch,
   searchBooksByName,
   searchBooksByRent,
@@ -12,5 +13,7 @@ router.route("/allbooks").get(getAllBooks);
 router.route("/search/name").get(searchBooksByName);
 router.route("/search/rent").get(searchBooksByRent);
 router.route("/search/global").get(globalBookSearch);
+
+router.route("/categories").get(getAllCategories);
 
 export default router;
