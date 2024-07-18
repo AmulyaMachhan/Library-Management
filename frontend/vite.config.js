1e51a055d7b32eaa685305ab3340518dfbfa883f
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-18 22:33:34 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "https://library-management-nu-seven.vercel.app/",
-    },
-  },
+  server: {},
 });
