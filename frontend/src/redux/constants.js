a9a958d64caee1b6b88b8027efaa18934ca7b1e7
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-18 16:33:34 +0530

    Updated

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://library-management-nu-seven.vercel.app/";
 export const USERS_URL = "/api/v1/users";
 export const BOOKS_URL = "/api/v1/books";
 export const TRANSACTIONS_URL = "/api/v1/transactions";
